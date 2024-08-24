d32fad33c6656c90996a66993c3d9a32e3d3b6f7
foselin <foselinalvarez@gmail.com>
2024-08-24 13:06:18 -0600

    MiParcialitoC

---
+++ MiParcialitoC/MiParcialitoC/Controllers/ParcialController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MiParcialitoC.Controllers
+{
+    public class ParcialController:Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Enter()
+        {
+            return View();
+        }
+    }
+}
